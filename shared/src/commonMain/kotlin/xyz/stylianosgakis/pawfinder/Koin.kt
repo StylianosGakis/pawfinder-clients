82b509e9f84008a4dfe6babe4c2bc35cd52047b9
stylianosgakis <stylianos.gakis98@gmail.com>
2022-02-16 03:11:55 +0200

    Add necessary code to start using Koin on iOS

--- shared/src/commonMain/kotlin/xyz/stylianosgakis/pawfinder/Koin.kt
+++ shared/src/commonMain/kotlin/xyz/stylianosgakis/pawfinder/Koin.kt
@@ -3,22 +3,34 @@
 package xyz.stylianosgakis.pawfinder
 
 import com.apollographql.apollo3.ApolloClient
+import kotlin.reflect.KClass
+import org.koin.core.Koin
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 import xyz.stylianosgakis.pawfinder.backend.GetAnimalsUseCase
 
 fun initKoin(
-    initCode: KoinApplication.() -> Unit = {},
+    extraConfiguration: KoinApplication.() -> Unit = {},
 ) {
     startKoin {
-        initCode()
+        extraConfiguration()
         modules(sharedModule)
     }
 }
 
-val sharedModule: Module = module {
+@Suppress("unused") // Used from Swift
+fun initKoin() {
+    initKoin {}
+}
+
+internal fun <T> Koin.getDependency(clazz: KClass<*>): T {
+    return get(clazz, parameters = { parametersOf(clazz.simpleName) }) as T
+}
+
+private val sharedModule: Module = module {
     single<ApolloClient> {
         ApolloClient.Builder()
             .serverUrl(BuildKonfig.backendGraphQlEndpoint)
